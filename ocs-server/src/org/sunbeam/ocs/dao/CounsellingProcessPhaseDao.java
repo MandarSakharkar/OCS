8dc3915b4a76befeaa35a633a06d43753c179ac6
Mandar Sakharkar <mandarsakharkar@gmail.com>
2018-03-08 20:25:58 +0530

    Revert "second commit of client excluding server"
    This reverts commit 9a8237dc9df52142c5cf78f90c4e8955e72981a8.
    
    deleted server commit

--- ocs-server/src/org/sunbeam/ocs/dao/CounsellingProcessPhaseDao.java
+++
@@ -1,8 +0,0 @@
-package org.sunbeam.ocs.dao;
-
-import org.sunbeam.ocs.entities.CounsellingProcessPhase;
-
-public interface CounsellingProcessPhaseDao {
-	CounsellingProcessPhase getCounsellingProcessPhase();
-	int setCounsellingProcessPhase(CounsellingProcessPhase counsellingProcessPhase);
-}
